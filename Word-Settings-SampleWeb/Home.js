55690fb83fc1701bb3671d7dbba70ea5d5453e86
Marc LaFleur <marcla@microsoft.com>
2017-04-25 10:20:48 -0400

    Add project files.

---
+++ Word-Settings-SampleWeb/Home.js
@@ -0,0 +1,42 @@
+﻿/// <reference path="/Scripts/FabricUI/MessageBanner.js" />
+
+
+(function () {
+    "use strict";
+
+    var messageBanner;
+
+    // The initialize function must be run each time a new page is loaded.
+    Office.initialize = function (reason) {
+        $(document).ready(function () {
+            // Add a click event handler for the highlight button.
+            $('#run-button').click(runButton);
+        });
+    };
+
+
+
+    function runButton() {
+        Word.run(function (context) {
+
+            return context.sync()
+                .then(function () {
+
+                    Office.context.document.settings.refreshAsync(function () {
+                        var foo = Office.context.document.settings.get('hello');
+                        if (!foo) {
+                            Office.context.document.settings.set('hello', 'world');
+                            Office.context.document.settings.saveAsync(function (asyncResult) {
+                                $('#content').html('Settings saved with status: ' + asyncResult.status);
+                            });
+                        }
+                        else {
+                            $('#content').html('Value found: ' + foo);
+                        }
+                    });
+
+                })
+                .then(context.sync);
+        });
+    }
+})();
---
+++ Word-Settings-SampleWeb/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Word_Settings_SampleWeb")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Word_Settings_SampleWeb")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fde750b1-7cee-4194-b789-e1a052049452")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
